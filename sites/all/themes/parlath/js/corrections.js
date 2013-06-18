faf88a2b015bb4208ba52c1fe25870c883600f6b
pup <pup@musica.nu>
2013-06-18 18:53:18 +0200

    Commit per 18th of June, site basically finished

---
+++ sites/all/themes/parlath/js/corrections.js
@@ -0,0 +1,29 @@
+(function($) {
+        $(document).ready(function(){
+            
+            $('div#views_slideshow_cycle_teaser_section_musician_cv-block').addClass("ert-art");
+  			$('div#views_slideshow_cycle_teaser_section_musician_cv-block').css('height', "");
+  			//$('div#views_slideshow_cycle_teaser_section_musician_cv-block').css('width', "700");
+
+        });
+    })(jQuery);
+    
+    
+ /*
+ (function ($) {
+  Drupal.behaviors.Corrections =  {
+    attach: function(context, settings) {
+
+      //Begin code
+      var myVarOne,
+          myVarTwo;
+
+  $('div#views_slideshow_cycle_teaser_section_musician_cv-block').addClass("ert-ert");
+  $('div#views_slideshow_cycle_teaser_section_musician_cv-block').css("height", 700px);
+
+     // End of code
+
+    }
+  };
+})(jQuery);
+*/
